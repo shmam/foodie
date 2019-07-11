43762a9365e8e7c3e4c40ecec6aff00187bb36b0
slarsen7 <samuel.larsen@optum.com>
2019-07-11 11:59:16 -0400

    fixing

--- runner.py
+++ runner.py
@@ -1,4 +1,14 @@
-from grindtime import app
+from flask import Flask
+
 # This is the first command that happens when we run this file
 if __name__ == "__main__":
+    app = Flask(__name__)
     app.run(debug=True)
+    
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
